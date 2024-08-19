308f70c3dad99ebb0b15000d84e75bff35754470
nguyenvanron210 <nguyenvanron210@gmail.com>
2024-08-19 09:26:50 +0700

    back-up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dashboard-dark">
+      <h1 className="logo">
+        <strong>ForShop</strong>
+      </h1>
+      <div className="navigation">
+        <span className="span1">Dashboard</span>
+        <span>Orders</span>
+        <span>Products</span>
+        <span>Marketing</span>
+        <span>Rates</span>
+        <span>Reports</span>
+      </div>
+      <div className="bg">
+        <h1 className="welcome">
+          Welcome, <strong>Damian</strong>
+        </h1>
+        <div className="top-actions">
+          <input placehoder="Search for products..."></input>
+          <button></button>
+        </div>
+        <div className="summary-stats">
+          <div className="orders">
+            <div className="retangle-3"></div>
+          </div>
+          <div className="profit">
+            <div className="retangle-3"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
